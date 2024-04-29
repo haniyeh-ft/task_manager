3196d29205a4273e9b8ef983b020fe0728129eac
hanie-ft <hanie.ft95@gmail.com>
2024-04-29 16:04:06 +0330

    Add Tasks model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+# status
+
+
+class Tasks(models.Model):
+    STATUS_CHOICES=(
+        ('in_progress' , 'In Progress'),
+        ('completed' , 'Completed')
+    )
+    title = models.CharField(max_length=300, blank=False) #index
+    detail = models.TextField( blank=True )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    # signe = user
+
+    def __str__(self) -> str:
+        return f'{self.title} - {self.status}'
